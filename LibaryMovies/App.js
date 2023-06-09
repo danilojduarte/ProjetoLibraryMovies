754b37c1cca0657bf8f7157f05787d8ceb281419
DaniloDuarte <danilojoseduarte@gmail.com>
2023-06-09 16:13:48 -0300

    Atualizando pastas do projeto

---
+++ LibaryMovies/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+function App(){
+  return(
+    <View style={{ marginTop: 100 }}>
+      <Text style={{ color: 'red', fontSize: 30 }}>Primeiro App</Text>
+    </View>
+  )
+}
+
+
+
+export default App;
