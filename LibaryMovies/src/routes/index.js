2dfe388742ac3865ecad1bf97f9e54c222b950b3
DaniloDuarte <danilojoseduarte@gmail.com>
2023-06-11 09:45:39 -0300

    Components Gerados, problemas com build

--- LibaryMovies/src/routes/index.js
+++ LibaryMovies/src/routes/index.js
@@ -1,18 +1,19 @@
+
 import React from "react";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 
-import Home from '../pages/Home';
-import Movies from '../pages/Movies';
+import Movies from '../pages/movies';
+import StackRoutes from './stackRoutes';
 
 const Drawer = createDrawerNavigator();
 
 function Routes(){
     return(
         <Drawer.Navigator>
-            <Drawer.Screen name="Home" component={Home} />
+            <Drawer.Screen name="HomeDrawer" component={StackRoutes} />
             <Drawer.Screen name="Movies" component={Movies} />
         </Drawer.Navigator>
     )
 }
 
-export default Drawer;
+export default Routes;
