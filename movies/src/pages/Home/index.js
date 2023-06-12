b5fe1b982eb2a8377508eeb85869530c53f9775e
DaniloDuarte <danilojoseduarte@gmail.com>
2023-06-12 14:40:04 -0300

    Criando novos componentes

---
+++ movies/src/pages/Home/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+function Home(){
+    return(
+        <View>
+            <Text>Danilo Duarte - Tela Home</Text>
+        </View>
+    )
+}
+
+export default Home;
